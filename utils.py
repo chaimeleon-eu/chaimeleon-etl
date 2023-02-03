8451de6199310a4b6e00f1b744a359f255334e34
Manuel Fdez <manufdh@hotmail.com>
2023-02-03 14:14:05 +0100

    added common functions for data mappings

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import numpy as np
 from typing import Union
 
 
@@ -59,3 +59,29 @@ def deduplicate_columns(df: pd.DataFrame) -> pd.DataFrame:
         new_columns.append(newitem)
     df.columns = new_columns
     return df
+
+def generate_wide_df(columns, concepts):
+    resDict = {}
+    for index,col in enumerate(columns):
+        for con in concepts:
+            resDict[col+'_'+con] = concepts[con][index]
+                        
+    return pd.DataFrame(data=resDict)
+
+def map_multi_concept(df: pd.DataFrame,columns, concepts):
+    df = df.copy()
+    wide_df = generate_wide_df(columns, concepts)
+    
+    n = len(df) + 1
+    wide_df =wide_df.assign(
+        person_id=np.arange(1, n),
+    )
+    value_vars = [[x for x in wide_df.columns if column in x] for column in columns]
+    
+    return multi_melt(
+            df=wide_df,
+            id_vars=['person_id'],
+            value_vars=value_vars,
+            var_name=[x[0] for x in columns],
+            value_name=columns
+        )
